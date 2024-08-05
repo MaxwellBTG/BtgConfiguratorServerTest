2ba68499e62e1125a15e1365c1abb21f2af39af5
MaxwellBTG <maxwell@bendtechgroup.com.au>
2024-08-05 11:23:38 +0800

    fixed on user credential

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const fs = require('node:fs');
 const dotenv = require('dotenv');
 dotenv.config();
 const port = process.env.PORT || 5000;
-const email = process.env.TEST_ID;
+const email = process.env.USER_ID;
 
 app.use(cors());
 app.use(express.json({limit:"25mb"}));
@@ -47,7 +47,7 @@ function sendEmail({
             port: 587,
             auth: {
                 user: email,
-                pass: process.env.TEST_PW
+                pass: process.env.USER_PW
             },
         })
 
